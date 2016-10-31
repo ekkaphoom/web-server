8556fdf21eb2cfff1f7a5736bb905f0fd67b237d
Ekkaphoom Phaisanphatthanasuk <ekkaphoom@gmail.com>
2016-10-31 08:05:55 +0700

    init repo

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+var middleware={
+    requireAuthentication:function(req,res,next){
+        console.log('private route hit!');
+        next();
+    },
+    logger:function(req,res,next){
+        console.log(req.method+" "+req.originalUrl);
+        next();
+    }
+}
+
+// app.use(middleware.requireAuthentication);
+ app.use(middleware.logger);
+
+app.get('/', function(req, res) {
+    res.send('Hello Express!');
+});
+app.get('/about',middleware.requireAuthentication, function(req, res) {
+    res.send('About Us')
+});
+app.use(express.static(__dirname + '/public'));
+app.use('/static', express.static(__dirname + '/public'));
+// console.log(__dirname+'/public');
+app.listen(port, function() {
+    console.log('starting app ' + port);
+});
