05553c8d35cdf8f173388947632300eb470cd162
Ekkaphoom Phaisanphatthanasuk <ekkaphoom@gmail.com>
2016-11-03 07:16:40 +0700

    change default page

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ var middleware=require('./middleware.js');
 // app.use(middleware.requireAuthentication);
  app.use(middleware.logger);
 
-app.get('/', function(req, res) {
-    res.send('Hello Express!');
-});
+// app.get('/', function(req, res) {
+//     res.send('Hello Express!');
+// });
+app.use(middleware.logger);
 app.get('/about',middleware.requireAuthentication, function(req, res) {
     res.send('About Us')
 });
